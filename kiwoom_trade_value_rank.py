7745b36c138ef30bc50c97ce17bed19dfb02b785
myjakelee-creator <myjakelee@gmail.com>
2026-07-05 20:08:45 +0900

    Install ranking engine bridge in launcher

--- kiwoom_trade_value_rank.py
+++ kiwoom_trade_value_rank.py
@@ -1,5 +1,9 @@
 import sys
 
+from stockboard_ranking_runtime_patch import install_stockboard_ranking_engine_patch
+
+install_stockboard_ranking_engine_patch()
+
 from stockboard_server import main
 
 
