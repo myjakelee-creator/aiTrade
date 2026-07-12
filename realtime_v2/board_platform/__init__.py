37a76289191184ed451f10689727c38b1d27b49b
myjakelee-creator <myjakelee@gmail.com>
2026-07-12 19:27:01 +0900

    perf: install quote reuse optimization before profiling

--- realtime_v2/board_platform/__init__.py
+++ realtime_v2/board_platform/__init__.py
@@ -1,5 +1,6 @@
 """Shared Board Platform services for StockBoard, ThemeBoard and future boards."""
 
+from .fast_path_optimize import install as install_fast_path_optimize
 from .fast_path_profile import install as install_fast_path_profile
 from .http_patch import install as install_http_patch
 
@@ -7,6 +8,7 @@ from .http_patch import install as install_http_patch
 def install(base, large):
     install_http_patch(base, large)
     actual_module = getattr(large, "guarded", large)
+    install_fast_path_optimize(actual_module, base)
     install_fast_path_profile(actual_module, base)
 
 
