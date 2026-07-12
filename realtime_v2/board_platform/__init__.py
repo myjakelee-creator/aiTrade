08058171948ad8b7a0cb35a934265e636d6af8a7
myjakelee-creator <myjakelee@gmail.com>
2026-07-12 20:03:45 +0900

    perf: install fallback and session hold caches

--- realtime_v2/board_platform/__init__.py
+++ realtime_v2/board_platform/__init__.py
@@ -7,16 +7,21 @@ from .hot_path_cprofile import install as install_hot_path_cprofile
 from .http_patch import install as install_http_patch
 from .market_session_cache import install as install_market_session_cache
 from .model_lane_merge_optimize import install as install_model_lane_merge_optimize
+from .previous_daily_fast import install as install_previous_daily_fast
+from .session_metric_fast import install as install_session_metric_fast
 
 
 def install(base, large):
     import realtime_v2.display_hold_policy_patch as display_hold_module
     import realtime_v2.market_session as market_session_module
+    import realtime_v2.session_metric_hold_patch as session_metric_module
 
     install_market_session_cache(market_session_module)
     install_display_hold_fast(display_hold_module)
+    install_session_metric_fast(session_metric_module)
     install_http_patch(base, large)
     actual_module = getattr(large, "guarded", large)
+    install_previous_daily_fast(actual_module)
     install_fast_path_optimize(actual_module, base)
     install_model_lane_merge_optimize()
     install_fast_path_profile(actual_module, base)
