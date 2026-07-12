ac94b7c4a5dbbb7edabee206552d233ba5def6aa
myjakelee-creator <myjakelee@gmail.com>
2026-07-12 19:37:39 +0900

    perf: install in-place model merge before profiling

--- realtime_v2/board_platform/__init__.py
+++ realtime_v2/board_platform/__init__.py
@@ -3,12 +3,14 @@
 from .fast_path_optimize import install as install_fast_path_optimize
 from .fast_path_profile import install as install_fast_path_profile
 from .http_patch import install as install_http_patch
+from .model_lane_merge_optimize import install as install_model_lane_merge_optimize
 
 
 def install(base, large):
     install_http_patch(base, large)
     actual_module = getattr(large, "guarded", large)
     install_fast_path_optimize(actual_module, base)
+    install_model_lane_merge_optimize()
     install_fast_path_profile(actual_module, base)
 
 
