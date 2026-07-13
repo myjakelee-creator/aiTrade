8716cab1593c6b6332bb1b62330f40aabfda5a2f
myjakelee-creator <myjakelee@gmail.com>
2026-07-13 14:21:33 +0900

    test: lock opening-burst launcher defaults

--- tests/test_stockboard_v2_safe_launcher.py
+++ tests/test_stockboard_v2_safe_launcher.py
@@ -22,8 +22,8 @@ def _preflight_script() -> str:
 
 def test_large_launcher_uses_safe_powershell_entrypoint_and_preflight():
     wrapper = _wrapper()
-    assert "scripts\\stockboard_v2_large_safe.ps1" in wrapper
-    assert "scripts\\stockboard_v2_openapi_preflight.ps1" in wrapper
+    assert "scripts\stockboard_v2_large_safe.ps1" in wrapper
+    assert "scripts\stockboard_v2_openapi_preflight.ps1" in wrapper
     assert "Stop-OpenApiStarterArtifacts" not in wrapper
 
 
@@ -33,6 +33,16 @@ def test_start_flow_stops_old_runtime_then_cleans_orphan_then_starts():
     preflight_index = wrapper.index('-File "%PREFLIGHT%"')
     start_index = wrapper.index('-Action "%START_ACTION%"')
     assert stop_index < preflight_index < start_index
+
+
+def test_wrapper_applies_safe_opening_burst_defaults_without_shrinking_universe():
+    wrapper = _wrapper()
+    assert 'STOCKBOARD_V2_COLLECTOR_LIMIT=100' in wrapper
+    assert 'STOCKBOARD_TRADE_VALUE_SAMPLE_MS=500' in wrapper
+    assert 'STOCKBOARD_HEAVY_SNAPSHOT_INTERVAL_MS=500' in wrapper
+    assert 'STOCKBOARD_HEAVY_SNAPSHOT_MAX_AGE_MS=2000' in wrapper
+    assert 'STOCKBOARD_STATUS_WRITE_INTERVAL_SEC=5' in wrapper
+    assert 'if not defined STOCKBOARD_V2_COLLECTOR_LIMIT' in wrapper
 
 
 def test_safe_launcher_waits_for_actual_login_and_realreg():
@@ -79,7 +89,7 @@ def test_preflight_blocks_another_openapi_python_host():
     script = _preflight_script()
     assert "Get-OtherOpenApiPythonRows" in script
     assert "kiwoom_interface_32" in script
-    assert "interfaces\\\\kiwoom" in script
+    assert "interfaces\\kiwoom" in script
     assert "Another 32-bit Kiwoom/OpenAPI Python host is running" in script
 
 
