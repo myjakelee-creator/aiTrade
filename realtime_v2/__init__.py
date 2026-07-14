6e8358994852ed1db3980be426c6e021a62e9c17
myjakelee-creator <myjakelee@gmail.com>
2026-07-14 15:59:35 +0900

    fix: install global StockBoard header sorting

--- realtime_v2/__init__.py
+++ realtime_v2/__init__.py
@@ -5,8 +5,10 @@ from __future__ import annotations
 # layout controls without adding TR/OpenAPI work.
 from realtime_v2.theme_average_view_layout_patch import install_runtime_wrappers
 from realtime_v2.theme_average_view_script_hotfix import install_script_hotfix
+from realtime_v2.stockboard_global_sort_patch import install as install_stockboard_global_sort
 
 install_runtime_wrappers()
 install_script_hotfix()
+install_stockboard_global_sort()
 
 __all__: list[str] = []
