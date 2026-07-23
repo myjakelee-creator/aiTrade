8e7d5ca56eb5b8e79905c970fe2e25d3d4ace58e
myjakelee-creator <myjakelee@gmail.com>
2026-07-23 11:57:10 +0900

    fix: install price-fast SSE after worker wrappers

--- realtime_v2/__init__.py
+++ realtime_v2/__init__.py
@@ -36,10 +36,14 @@ from realtime_v2.price_time_monotonic_patch import (
 from realtime_v2.sse_latest_only_patch import (
     install_runtime_wrapper as install_sse_latest_only,
 )
+from realtime_v2.price_fast_sse_patch import (
+    install_runtime_wrapper as install_price_fast_sse,
+)
 
 install_premarket_rollover_priority()
 install_price_time_monotonic()
 install_sse_latest_only()
+install_price_fast_sse()
 install_runtime_wrappers()
 install_script_hotfix()
 install_candidate_json_eight_criteria()
