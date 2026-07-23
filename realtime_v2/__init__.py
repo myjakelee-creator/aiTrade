fe2470e2ea8105ee4d4e50f183cac4c6cf4c644c
myjakelee-creator <myjakelee@gmail.com>
2026-07-23 13:46:55 +0900

    wire full stream relief policy into runtime

--- realtime_v2/__init__.py
+++ realtime_v2/__init__.py
@@ -45,6 +45,9 @@ from realtime_v2.price_fast_sse_patch import (
 from realtime_v2.price_fast_sse_recovery_patch import (
     install_runtime_wrapper as install_price_fast_sse_recovery,
 )
+from realtime_v2.full_stream_relief_patch import (
+    install_runtime_wrapper as install_full_stream_relief,
+)
 from realtime_v2.ratio_connection_cleanup_patch import (
     install_runtime_wrapper as install_ratio_connection_cleanup,
 )
@@ -55,6 +58,7 @@ install_price_time_monotonic()
 install_sse_latest_only()
 install_price_fast_sse()
 install_price_fast_sse_recovery()
+install_full_stream_relief()
 install_ratio_connection_cleanup()
 install_runtime_wrappers()
 install_script_hotfix()
