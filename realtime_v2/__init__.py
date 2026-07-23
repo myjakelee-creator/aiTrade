1e1a07e0d9fa0f9c17c354d3293365b0df1fa995
myjakelee-creator <myjakelee@gmail.com>
2026-07-23 14:02:55 +0900

    wire lightweight metric SSE into runtime

--- realtime_v2/__init__.py
+++ realtime_v2/__init__.py
@@ -48,6 +48,9 @@ from realtime_v2.price_fast_sse_recovery_patch import (
 from realtime_v2.full_stream_relief_patch import (
     install_runtime_wrapper as install_full_stream_relief,
 )
+from realtime_v2.metric_fast_sse_patch import (
+    install_runtime_wrapper as install_metric_fast_sse,
+)
 from realtime_v2.ratio_connection_cleanup_patch import (
     install_runtime_wrapper as install_ratio_connection_cleanup,
 )
@@ -59,6 +62,7 @@ install_sse_latest_only()
 install_price_fast_sse()
 install_price_fast_sse_recovery()
 install_full_stream_relief()
+install_metric_fast_sse()
 install_ratio_connection_cleanup()
 install_runtime_wrappers()
 install_script_hotfix()
